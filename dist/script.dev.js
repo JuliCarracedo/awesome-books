e43d1e98d763ebb7c2323d5c2bc6be79e52c8950
Aduda-Boaz <adudaboaz@gmail.com>
2021-08-10 01:46:17 +0300

    Modify the localStorage

--- dist/script.dev.js
+++ dist/script.dev.js
@@ -1,7 +1,7 @@
 "use strict";
 
 // let books = document.getElementsByClassName("books");
-// let collection = []
+var collection = JSON.parse(window.localStorage.getItem('collection')) || []; // let collection = [];
 // let submit = document.getElementById("submit")
 // let aTitle = document.getElementById("title")
 // let anAuthor = document.getElementById("author")
@@ -13,18 +13,33 @@
 //     books[0].appendChild(newBook)
 //     collection.push({title: title, author: author});
 // }
-var newBook = [];
 
-function addBook() {
-  var book = {};
-  book.title = document.getElementById('title').value;
-  book.author = document.getElementById('author').value;
-  displayBook();
-  saveBook();
-  newBook.push(book);
+var books = document.getElementsByClassName("books");
+
+for (book in collection) {
+  var thisBook = JSON.parse(book);
+  var newBook = document.createElement('div');
+  newBook.innerHTML = '<hr><h3>' + thisBook["title"] + '</h3><h3>' + thisBook["author"] + '</h3><button>Remove</button>';
+  books[0].appendChild(newBook);
 }
 
-function displayBook() {
-  var list = document.getElementById('list');
-  list.innerHTML = '';
+var form = document.forms[0];
+form.addEventListener('submit', function (event) {
+  event.preventDefault();
+  var book = {
+    'title': document.getElementById('title').value,
+    'author': document.getElementById('author').value
+  };
+  collection.push(book);
+  window.localStorage.setItem('collection', JSON.stringify(collection));
+  displayBook(book);
+  console.log(collection);
+});
+
+function displayBook(book) {
+  var newBook = document.createElement('div');
+  newBook.innerHTML = '<hr><h3>' + book.title + '</h3><h3>' + book.author + '</h3><button>Remove</button>';
+  books[0].appendChild(newBook);
 }
+
+console.log(window.localStorage);
--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 // let books = document.getElementsByClassName("books");
-let collection = [];
+let collection = JSON.parse(window.localStorage.getItem('collection')) || [] 
+// let collection = [];
 
 // let submit = document.getElementById("submit")
 // let aTitle = document.getElementById("title")
@@ -15,10 +16,10 @@ let collection = [];
 //     collection.push({title: title, author: author});
 // }
 let books = document.getElementsByClassName("books");
-
 for (book in collection) {
+    let thisBook = JSON.parse(book)
     let newBook = document.createElement('div');
-    newBook.innerHTML = '<hr><h3>'+ book.title + '</h3><h3>'+ book.author + '</h3><button>Remove</button>';
+    newBook.innerHTML = '<hr><h3>'+ thisBook["title"]  + '</h3><h3>'+ thisBook["author"]  + '</h3><button>Remove</button>';
     books[0].appendChild(newBook);
 }
   
@@ -32,7 +33,7 @@ form.addEventListener('submit', (event) => {
         'author' : document.getElementById('author').value
     };
     collection.push(book);
-    window.localStorage.setItem('collection', collection);
+    window.localStorage.setItem('collection', JSON.stringify(collection));
     displayBook(book);
     console.log(collection);
 
