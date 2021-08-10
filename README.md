6f903fc713b3ae8c422d7efc715a0f4df1891fd7
Aduda-Boaz <adudaboaz@gmail.com>
2021-08-10 17:27:59 +0300

    Modify the ReadMe file

--- README.md
+++ README.md
@@ -1,2 +1,55 @@
-# awesome-books
-'Awesome books' is a website where you can add or remove books from a collection of your own.
+![](https://img.shields.io/badge/Microverse-blueviolet)
+
+# Awesome Books
+
+> In this project, you will build a basic website that allows users to add/remove books from a list. You will achieve that by using JavaScript objects and arrays. You will also need to dynamically modify the DOM and add basic events.
+
+## Built With
+
+- JavaScript
+- HTML
+- VS Code
+
+## Authors
+
+👤 **Julian Carracedo**
+
+- GitHub: [@JuliCarracedo](https://github.com/JuliCarracedo)
+- Twitter: [@CarracedoTrigo](https://twitter.com/CarracedoTrigo)
+- LinkedIn: [LinkedIn](https://www.linkedin.com/in/julian-carracedo)
+
+👤 **Boaz Aduda**
+
+- GitHub: [@Aduda-Boaz](https://github.com/Aduda-Boaz)
+- Twitter: [Twitter](https://twitter.com/BoazAduda)
+
+## Getting Started
+
+- You will not need additional instructions on installing Prerequisites.
+
+### Prerequisites
+
+- No prerequisites needed for this project.
+
+## Usage
+
+- Practice basic UI usage with Javascript objects.
+
+## Contributing
+
+Contributions are what make the open source community such an amazing place to be learn, inspire, and create. Any contributions you make are **greatly appreciated**.
+
+1. Fork the Project
+2. Create your Feature Branch (`git checkout -b feature/NewFeature`)
+3. Commit your Changes (`git commit -m 'Add some NewFeature'`)
+4. Push to the Branch (`git push origin feature/NewFeature`)
+5. Open a Pull Request
+
+## License
+
+MIT License
+
+## Acknowledgements
+
+- [Microverse](https://www.microverse.org)
+- [GitHub Pages](https://pages.github.com)
--- dist/script.dev.js
+++ dist/script.dev.js
@@ -1,45 +1,44 @@
 "use strict";
 
-// let books = document.getElementsByClassName("books");
-var collection = JSON.parse(window.localStorage.getItem('collection')) || []; // let collection = [];
-// let submit = document.getElementById("submit")
-// let aTitle = document.getElementById("title")
-// let anAuthor = document.getElementById("author")
-// submit.addEventListener('click', addBook(aTitle, anAuthor))
-// function addBook(title, author){
-//     console.log("creating" + title)
-//     let newBook = document.createElement('div');
-//     newBook.innerHTML = '<hr><h3>'+ title + '</h3><h3>'+ author + '</h3><button>Remove</button>';
-//     books[0].appendChild(newBook)
-//     collection.push({title: title, author: author});
-// }
+/* eslint-disable no-unused-vars */
 
-var books = document.getElementsByClassName("books");
+/* eslint-disable no-use-before-define */
 
-for (book in collection) {
-  var thisBook = JSON.parse(book);
+/* eslint-disable no-plusplus */
+
+/* eslint-disable no-undef */
+var collection = JSON.parse(localStorage.getItem('collection')) || [];
+var books = document.getElementsByClassName('books');
+
+for (i = 0; i < collection.length; i++) {
+  thisBook = collection[i];
   var newBook = document.createElement('div');
-  newBook.innerHTML = '<hr><h3>' + thisBook["title"] + '</h3><h3>' + thisBook["author"] + '</h3><button>Remove</button>';
+  newBook.innerHTML = "<hr><h3>".concat(thisBook.title, "</h3><h3>").concat(thisBook.author, "</h3><button id=\"").concat(i, "\" onclick=\"removeElement(").concat(i, ")\" >Remove</button>");
   books[0].appendChild(newBook);
 }
 
-var form = document.forms[0];
+var form = document.forms[0]; // Event listener for the form. Saves input data and tells displayBook() to add a div
+
 form.addEventListener('submit', function (event) {
   event.preventDefault();
   var book = {
-    'title': document.getElementById('title').value,
-    'author': document.getElementById('author').value
+    title: document.getElementById('title').value,
+    author: document.getElementById('author').value
   };
   collection.push(book);
   window.localStorage.setItem('collection', JSON.stringify(collection));
   displayBook(book);
-  console.log(collection);
-});
+}); // commanded by the Listener, adds a div with a book's data and a remove button
 
 function displayBook(book) {
   var newBook = document.createElement('div');
-  newBook.innerHTML = '<hr><h3>' + book.title + '</h3><h3>' + book.author + '</h3><button>Remove</button>';
+  newBook.innerHTML = "<hr><h3>".concat(book.title, "</h3><h3>").concat(book.author, "</h3><button>Remove</button>");
   books[0].appendChild(newBook);
-}
+} // called by clicking any remove button. Deletes said element
 
-console.log(window.localStorage);
+
+function removeElement(i) {
+  collection.splice(i, 1);
+  window.localStorage.setItem('collection', JSON.stringify(collection));
+  window.location.reload();
+}
