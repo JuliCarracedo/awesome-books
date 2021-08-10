eaf52c01505ff110a3c265268c9d8b0ba114a7f7
JuliCarracedo <julicarra@hotmail.com>
2021-08-10 10:37:56 -0300

    Add remove function and fix storage system

--- script.js
+++ script.js
@@ -1,30 +1,18 @@
-// let books = document.getElementsByClassName("books");
-let collection = JSON.parse(window.localStorage.getItem('collection')) || [] 
-// let collection = [];
 
-// let submit = document.getElementById("submit")
-// let aTitle = document.getElementById("title")
-// let anAuthor = document.getElementById("author")
+let collection = JSON.parse(localStorage.getItem('collection')) || [] 
 
-// submit.addEventListener('click', addBook(aTitle, anAuthor))
-
-// function addBook(title, author){
-//     console.log("creating" + title)
-//     let newBook = document.createElement('div');
-//     newBook.innerHTML = '<hr><h3>'+ title + '</h3><h3>'+ author + '</h3><button>Remove</button>';
-//     books[0].appendChild(newBook)
-//     collection.push({title: title, author: author});
-// }
 let books = document.getElementsByClassName("books");
-for (book in collection) {
-    let thisBook = JSON.parse(book)
+
+for (i = 0; i<collection.length; i++ ) {
+    thisBook = collection[i]
     let newBook = document.createElement('div');
-    newBook.innerHTML = '<hr><h3>'+ thisBook["title"]  + '</h3><h3>'+ thisBook["author"]  + '</h3><button>Remove</button>';
+    newBook.innerHTML = '<hr><h3>'+ thisBook["title"]  + '</h3><h3>'+ thisBook["author"]  + '</h3><button id="'+i+'" onclick="removeElement('+i+')" >Remove</button>';
     books[0].appendChild(newBook);
 }
   
 let form = document.forms[0];
 
+// Event listener for the form. Saves input data and tells displayBook() to add a div 
 form.addEventListener('submit', (event) => {
     event.preventDefault();
 
@@ -33,17 +21,26 @@ form.addEventListener('submit', (event) => {
         'author' : document.getElementById('author').value
     };
     collection.push(book);
+   
     window.localStorage.setItem('collection', JSON.stringify(collection));
     displayBook(book);
     console.log(collection);
 
 });
 
+// commanded by the Listener, adds a div with a book's data and a remove button
 function displayBook(book) {
     let newBook = document.createElement('div');
     newBook.innerHTML = '<hr><h3>'+ book.title + '</h3><h3>'+ book.author + '</h3><button>Remove</button>';
     books[0].appendChild(newBook);
 }
-console.log(window.localStorage)
+
+// called by clicking any remove button. Deletes said element
+function removeElement(i){
+    collection.splice(i, 1);
+    window.localStorage.setItem('collection', JSON.stringify(collection));
+    window.location.reload();
+}
+
 
 
